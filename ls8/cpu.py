faf8287e26280536534f72c323d95bdbaa69af14
Annette Odingo <odingoannette@gmail.com>
2020-09-13 16:29:24 +0300

    Fix bug

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,5 +1,8 @@
 """CPU functionality."""
-
+HLT = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
+MUL = 0b10100010
 import sys
 
 class CPU:
@@ -10,11 +13,12 @@ class CPU:
         self.reg =  [0] * 8
         self.pc = 0
         self.ram = [0]* 256
+        self.running = True
 
     def load(self):
         """Load a program into memory."""
-
-        address = 0
+        try:
+            address = 0
 
         # For now, we've just hardcoded a program:
 
@@ -31,14 +35,18 @@ class CPU:
         # for instruction in program:
         #     self.ram[address] = instruction
         #     address += 1
-        with open(sys.argv[1]) as f:
-            for line in f:
-                val = line.split("#")[0].strip()
-                if val == " ":
-                    continue
-                intVal = int(val, 2)
-                self.ram[address] = intVal
-                address += 1
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    val = line.split("#")[0].strip()
+                    if val == "":
+                        continue
+                    intVal = int(val, 2)
+                    self.ram[address] = intVal
+                    address += 1
+
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {sys.argv[1]} not found!")
+            sys.exit(2)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -76,22 +84,18 @@ class CPU:
         self.ram[address] = value
     def run(self):
         """Run the CPU."""
-        HLT = 0b00000001
-        LDI = 0b00000010
-        PRN = 0b01000111
-        running = True
-        self.ir = self.ram_read(self.pc)
-        reg_num1 = self.ram_read(self.pc + 1)
-        reg_num2 = self.ram_read(self.pc + 2)
-        while running:
-            if self.ir == LDI:
+        
+        while self.running:
+            ir = self.ram[self.pc]
+            instruction_length = ((ir >> 6) & 0b11) + 1
+            reg_num1 = self.ram_read(self.pc + 1)
+            reg_num2 = self.ram_read(self.pc + 2)
+            if ir == HLT:
+                self.running = False 
+            elif ir == LDI:
                 self.reg[reg_num1] = reg_num2
-                self.pc += 3
-            elif self.ir == PRN:
+            elif ir == PRN:
                 print(self.reg[reg_num1])
-                self.pc += 2
-            elif self.ir == HLT:
-                running = False
-            else:
-                print("Unknown Instruction!")
-                sys.exit(1)
+            elif ir == MUL:
+                self.alu("MUL", reg_num1, reg_num2)
+            self.pc += instruction_length
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -3,9 +3,13 @@
 """Main."""
 
 import sys
-from cpu import *
-
+from cpu import CPU
+if len(sys.argv) != 2:
+    print(f"usage: {sys.argv[0]} <filename>")
+    sys.exit(1)
 cpu = CPU()
 
 cpu.load()
 cpu.run()
+
+
